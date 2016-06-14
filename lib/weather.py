50624f7627c01e7ff15492f29409a56ad0e12e9f
Dan Nicholson <daniel.nicholson@sage.com>
2016-06-14 21:19:03 +0100

    correct location

--- lib/weather.py
+++ lib/weather.py
@@ -11,7 +11,10 @@ import pyowm
 def weather_openweathermap(self, key):
 	owm = pyowm.OWM(key)
 
-	# Search for current weather in London (UK)
-	observation = owm.weather_around_coords(-1.69, 55.17)
-	w = observation.get_weather()
-	return (w.get_status() + '. ' + format(w.get_temperature('celsius')['temp'], '.1f')  + 'degrees')
+	try:
+		observation = owm.weather_at_id(2642182)
+		w = observation.get_weather()
+		l = observation.get_location()
+		return ('Weather in ' + l.get_name() + ': ' + w.get_detailed_status() + '. ' + format(w.get_temperature('celsius')['temp'], '.1f')  + 'degrees')
+	except Exception as e:
+		return e
