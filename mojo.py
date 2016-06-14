089f40bf6e72c55ce3052f6ca34417b122445e53
Dan Nicholson <daniel.nicholson@sage.com>
2016-06-14 19:37:52 +0100

    debug

--- mojo.py
+++ mojo.py
@@ -62,10 +62,10 @@ class Mojo(telepot.Bot):
 
     # Handle messages from users
     def handle(self, msg):
-        self.adminMessage(msg['chat']['id'])
+        #self.adminMessage(msg['chat']['id'])
         try:
             if msg['chat']['id'] not in self.config.get('Config','Users').split(','):
-                self.adminMessage('Unauthorized access attempt by: ' + msg['chat']['id'])
+                self.adminMessage('Unauthorized access attempt by: ' + str(msg['chat']['id']))
                 return
         except Exception as e:
             self.adminMessage(str(e))
