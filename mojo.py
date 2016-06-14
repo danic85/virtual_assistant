0b23e600dc39dcaacbe270e683681ab08ca1f1c1
Dan Nicholson <daniel.nicholson@sage.com>
2016-06-14 13:38:28 +0100

    Add datetime to transactions

--- mojo.py
+++ mojo.py
@@ -222,7 +222,7 @@ class Mojo(telepot.Bot):
         expense = self.command.split(' ', 1)
         with open('expenses.csv', 'a') as csvfile:
             csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            csvwriter.writerow([expense[1], float(expense[0].strip()) * -1])
+            csvwriter.writerow([expense[1], float(expense[0].strip()) * -1, datetime.datetime.now()])
         return 'Logged expense: ' + str(self.command) + "\n" + self.expenses_remaining()
         
 bot = Mojo()
