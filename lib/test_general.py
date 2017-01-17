f0852c6f1ee3bd27f306c0f8a42e2dda7d629963
Dan Nicholson <daniel.nicholson@sage.com>
2017-01-17 21:08:02 +0000

    Updated tests

--- lib/test_general.py
+++ lib/test_general.py
@@ -6,8 +6,6 @@ class TestGeneralMethods(unittest.TestCase):
 
     def test_time(self):
         self.assertEqual(time(self), datetime.datetime.now().strftime('%I:%M %p'))
-    def test_time_fail(self):
-        self.assertEqual(time(self), 'bob')
 
 if __name__ == '__main__':
     unittest.main()
