b79c1d4eb1695702a89a816d36f0f717ecdcdb79
Dan Nicholson <daniel.nicholson@sage.com>
2017-01-19 21:56:18 +0000

    Fix grep for mojo

--- restart_script.sh
+++ restart_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
 # Restart script if not running (error caused failure)
-if pgrep -lf python | grep mojo.py ; then
+if pgrep -f mojo.py ; then
  :
 else 
   sudo python /home/pi/mojo_home_bot/mojo.py &
