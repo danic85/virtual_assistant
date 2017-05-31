0e4f488b120506f4a06710410b533cd2f051008a
Dan Nicholson <daniel.nicholson@sage.com>
2017-05-31 23:04:07 +0100

    DB supports find_one and find_random

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 # from pymongo import MongoClient
 import pymongo
-
+import random
 
 class Database(object):
     def __init__(self):
@@ -11,6 +11,13 @@ class Database(object):
         data_json['collection'] = collection_name  # this will mean we don't need to pass it explicitly to delete
         result = self.db[collection_name].insert_one(data_json)
         return result.inserted_id
+
+    def find_random(self, collection_name, criteria={}):
+        results = self.db[collection_name].find(criteria)
+        return results.skip(random.randrange(0, results.count(), 1)).next()
+
+    def find_one(self, collection_name, criteria={}):
+        return self.db[collection_name].find_one(criteria)
 
     def find(self, collection_name, criteria={}, sort=[]):
         results = self.db[collection_name].find(criteria)
