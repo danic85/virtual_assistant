3d52f58f14e7e770b70bd50be183b3d7e084c1f1
Dan Nicholson <daniel.nicholson@sage.com>
2017-06-08 20:33:04 +0100

    Unit tests

--- behaviours/tests/test_translator.py
+++ behaviours/tests/test_translator.py
@@ -22,6 +22,20 @@ class TestTranslatorMethods(unittest.TestCase):
         b.match = re.search(key, 'translate this is a test to spanish', re.IGNORECASE)
         self.assertEqual(b.translate(), "Esto Es un prueba")
 
+    def test_translate_back(self):
+        path = os.path.dirname(os.path.realpath(__file__)) + '/testdata/translator_get_language_code.json'
+        with open(path) as data_file:
+            data = json.load(data_file)
+
+        translate = {"to": "en", "translationText": "This is a test", "from": "es", "text": "Esto Es un prueba"}
+        feeds.get_json = Mock()
+        feeds.get_json.side_effect = [data, translate]
+
+        b = translator.Translator(db=None, config={}, dir='')
+        key, value = next(iter(b.routes.items()))
+        b.match = re.search(key, 'translate Esto Es un prueba from spanish', re.IGNORECASE)
+        self.assertEqual(b.translate(), "This is a test")
+
     def test_translate_no_language(self):
         path = os.path.dirname(os.path.realpath(__file__)) + '/testdata/translator_get_language_code.json'
         with open(path) as data_file:
