70e56be032345a4b46962af34c93aafc9a6d8bf9
Dan Nicholson <daniel.nicholson@sage.com>
2017-06-08 20:57:43 +0100

    Unit tests

--- behaviours/tests/test_news.py
+++ behaviours/tests/test_news.py
@@ -37,6 +37,23 @@ class TestNewsMethods(unittest.TestCase):
         self.assertEqual(g.top_stories()[0:30], "Election 2017 live updates: Th")
 
     @freeze_time("2017-06-07")
+    def test_top_stories_default_sort(self):
+        path = os.path.dirname(os.path.realpath(__file__)) + '/testdata/news_top_stories.json'
+        with open(path) as data_file:
+            data = json.load(data_file)
+
+        path = os.path.dirname(os.path.realpath(__file__)) + '/testdata/news_news_sources.json'
+        with open(path) as data_file:
+            sources = json.load(data_file)
+
+        feeds.get_json = Mock()
+        feeds.get_json.side_effect = [sources, data]
+        config = Mock()
+        config.get = Mock(return_value='nomatch')
+        g = news.News(db=None, config=config, dir='')
+        self.assertEqual(g.top_stories()[0:30], "Election 2017 live updates: Th")
+
+    @freeze_time("2017-06-07")
     def test_top_stories_error(self):
 
         data = {'status': 'error', 'message': 'error!'}
