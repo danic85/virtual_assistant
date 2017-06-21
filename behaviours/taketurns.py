dcf39e002d2d729c6bf2b5eb874a35ee79695eae
Dan Nicholson <daniel.nicholson@sage.com>
2017-06-21 19:23:12 +0100

    Updated regex

--- behaviours/taketurns.py
+++ behaviours/taketurns.py
@@ -8,11 +8,12 @@ from datetime import date
 class Taketurns(Behaviour):
 
     routes = {
-        "who's (night|turn) is it": 'turn'
+        "(?:who's|is it my) (night|turn) (?:is it|tonight)(?:\?)?": 'turn'
     }
 
     def turn(self):
         # if it is monday or an odd number of days since the given date
+        # @todo this is a placeholder for a more dynamic piece of functionality
         if date.today().weekday() != 0:
             d0 = date(2017, 1, 1)
             delta = date.today() - d0
