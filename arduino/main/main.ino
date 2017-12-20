f97117ac78b9a3f59919cf72791577b93230f3f7
Dan Nicholson <daniel.nicholson@sage.com>
2017-12-20 19:12:47 +0000

    Encoder test

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -12,10 +12,25 @@ HCSR04 distance1 = HCSR04(A3,A4);
 HCSR04 distance2 = HCSR04(A1,A2);
 //InfraRed ir;
 
+// Encoder Test
+int DL0 = 3, DL1 = 5, DR0 = 2, DR1 = 4; // Pins for Right and Left encoders. DL0 and DR0 must be Interrupts
+int lStat0, lStat1, rStat0, rStat1;
+int lpos = 0, rpos = 0;  // variables keeping count of the pulses on each side
+String dir = "None";
+
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  motors.doAction(DIRECTION_FORWARDS, MOTOR_SPEED_FULL);
+  //Serial.begin(9600);
+
+  pinMode(DL0, INPUT);
+  pinMode(DL1, INPUT);
+  pinMode(DR0, INPUT);
+  pinMode(DR1, INPUT);
+  attachInterrupt(1, intLeft, RISING);
+  attachInterrupt(0, intRight, RISING);
+  Serial.begin(115200);
+
 }
 void loop() {
   // Adjust course if moving forwards and getting close to something, regardless of input
@@ -32,4 +47,26 @@ void loop() {
     if (response != "") speak.doAction(response);
     else speak.doAction(serialString);
   }
+}
+
+void intLeft()  // This interrupt routine runs if the left side encoder is triggered (DL0)
+{
+  lStat0 = digitalRead(DL0);
+  lStat1 = digitalRead(DL1);
+  if(lStat1 > 0) // use DL1 to determine which direction we're moving. HIGH = Forwards
+    lpos++;
+  else
+    lpos--;
+  dir = "Left";
+}
+
+void intRight()    // This interrupt routine runs if the right side encoder is triggered (DR0)
+{
+  rStat0 = digitalRead(DR0);
+  rStat1 = digitalRead(DR1);
+  if(rStat1 > 0) // use DR1 to determine which direction we're moving. HIGH = Backwards
+    rpos--;
+  else
+    rpos++;
+  dir = "Right";
 }
